6f291e41854a183bec68d346fe47f5ccf25ddb54
Hannah Young <hannah@corgibyte.com>
2022-01-19 13:28:54 -0800

    Implement all CRUD routes for stations controller
    Co-authored-by: Aaron Minnick <aaronminnick@users.noreply.github.com>
    Co-authored-by: Christinamawel <Christinamawel@users.noreply.github.com>

--- Trains/Controllers/StationsController.cs
+++ Trains/Controllers/StationsController.cs
@@ -25,5 +25,63 @@ namespace Trains.Controllers
       List<Station> stations = await _db.Stations.OrderBy(station => station.Name).ToListAsync();
       return new JsonResult(stations);
     }
+
+    [HttpPost]
+    public async Task<ActionResult<Station>> Post(Station station)
+    {
+      _db.Stations.Add(station);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction("Post", new { id = station.StationId }, station);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Station station)
+    {
+      if (id != station.StationId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(station).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!StationExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    private bool StationExists(int id)
+    {
+      return _db.Stations.Any(e => e.StationId == id);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteStation(int id)
+    {
+      var station = await _db.Stations.FindAsync(id);
+      if (station == null)
+      {
+        return NotFound();
+      }
+
+      _db.Stations.Remove(station);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
   }
 }
